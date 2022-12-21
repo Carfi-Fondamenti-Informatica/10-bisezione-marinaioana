f0e664c5beedecf39731c313662b8ed7190187d3
marinaioana <115020895+marinaioana@users.noreply.github.com>
2022-12-21 18:04:01 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
+float f( float a){
+    float y=0;
+    y= ((a*a)* cos(a))+1;
+    return y;
+}
+
 int main() {
-   return 0;
+    float a=0, b=0, m=0,err=1;
+    while((f(a)* f(b))>=0){
+        cout<<"inserire estremi"<<endl;
+        cin>>a>>b;
+    }
+
+
+    while (f(m)!=0){
+        m=((a+b)/2);
+        if ((f(m)*f(b))<0){
+            a=m;
+        }else{
+            b=m;
+        }
+        err=((b-a)/2);
+        if (err<(1e-6)){
+            break;
+        }
+    }
+
+    m=m*10000;
+    m=(int)m;
+    m=m/10000;
+    cout<< m;
+
+
+    return 0;
 }
